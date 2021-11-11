ef7dcf91a4df9e1f9b0dc2e7bf4eaaea1e068f9f
Daniela Williams <dani.elen.williams@gmail.com>
2021-11-11 19:28:41 -0300

    Update index.js

--- index.js
+++ index.js
@@ -10,12 +10,16 @@ app.use(cors("*"))
 
 app.use(express.json());
 app.use(urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, "public/build/static")));
+app.use(express.static(path.join(__dirname, "public/build")));
 
 app.set("views", path.join(__dirname, "views", "ejs"));
 app.set("view engine", "ejs");
 
 ServerRouter(app);
+
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'public/build', 'index.html'));
+});
 
 app.listen(config.port, (req, res, next) => {
   console.log(`App listening in port ${config.port}`);
